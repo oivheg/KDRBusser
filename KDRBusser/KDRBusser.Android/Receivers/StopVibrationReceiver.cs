26d2ce68b73d2efca00c0817d90b2d9fd18c2494
Øivind Heggland <oivind@kdrstavanger.no>
2017-10-10 13:18:00 +0200

    some nugets updated, not prelease,   NETStandar.Libary is not updatet from 2.0.0 to 2.0.1 because of some dependencies somewhere.

---
+++ KDRBusser/KDRBusser.Android/Receivers/StopVibrationReceiver.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using KDRBusser.Droid;
+
+namespace KDRBusser.Droid.Receivers
+{
+    [BroadcastReceiver(Enabled = true)]
+    [IntentFilter(new[] { "CancelVibrations" })]
+  public  class StopVibrationReceiver : BroadcastReceiver
+    {
+        
+      
+        
+        
+        public override void OnReceive(Context context, Intent intent)
+        {
+            // Do stuff here when device reboots.
+         
+
+            MyFirebaseMessagingService.timer.Stop();
+        }
+    }
+}
